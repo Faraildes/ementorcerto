d37a8723634cf1a43f537ff8ae919b2b860ab1d8
Faraildes <faraildesbarbosaferreira@gmail.com>
2024-08-29 20:22:29 -0300

    Adding database access

--- src/model/entities/Student.java
+++ src/model/entities/Student.java
@@ -4,28 +4,30 @@ import java.io.Serializable;
 import java.util.Date;
 import java.util.Objects;
 
-public class Teacher implements Serializable{	
+public class Student implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
 	
 	private Integer id;
-	private String name;	
+	private String name;
+	private Date birthDate;
 	private String cpf;
 	private String phone;
-	private Double salary;
-	
-	public Teacher() {
+	private String period;
+		
+	public Student() {	
 	}
 		
-	public Teacher(Integer id, String name, String cpf, String phone, Double salary) {
-		super();
+	public Student(Integer id, String name, Date birthDate, String cpf, String phone, String period) {
 		this.id = id;
-		this.name = name;		
+		this.name = name;
+		this.birthDate = birthDate;
 		this.cpf = cpf;
 		this.phone = phone;
-		this.salary = salary;
+		this.period = period;
 	}
-		
+	
+
 	public Integer getId() {
 		return id;
 	}
@@ -41,7 +43,15 @@ public class Teacher implements Serializable{
 	public void setName(String name) {
 		this.name = name;
 	}
-	
+
+	public Date getBirthDate() {
+		return birthDate;
+	}
+
+	public void setBirthDate(Date birthDate) {
+		this.birthDate = birthDate;
+	}
+
 	public String getCpf() {
 		return cpf;
 	}
@@ -58,12 +68,21 @@ public class Teacher implements Serializable{
 		this.phone = phone;
 	}
 
-	public Double getSalary() {
-		return salary;
+	public String getPeriod() {
+		return period;
 	}
 
-	public void setSalary(Double salary) {
-		this.salary = salary;
+	public void setPeriod(String period) {
+		this.period = period;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
 	}
 
 	@Override
@@ -74,15 +93,13 @@ public class Teacher implements Serializable{
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		Teacher other = (Teacher) obj;
+		Student other = (Student) obj;
 		return Objects.equals(id, other.id);
 	}
-
+	
 	@Override
 	public String toString() {
-		return "Teacher [id=" + id + ", name=" + name + ", cpf=" + cpf + ", phone=" + phone + ", salary=" + salary
-				+ "]";
-	}
-
-					
+		return "Student [id=" + id + ", name=" + name + ", birthDate=" + birthDate + ", cpf=" + cpf + ", phone=" + phone
+				+ ", period=" + period + "]";
+	}		
 }
--- src/model/services/TeacherService.java
+++ src/model/services/TeacherService.java
@@ -1,18 +1,16 @@
 package model.services;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import model.dao.DaoFactory;
+import model.dao.TeacherDao;
 import model.entities.Teacher;
 
 public class TeacherService {
 	
+	private TeacherDao dao = DaoFactory.createTeacherDao();
+	
 	public List<Teacher> findAll() {
-		List<Teacher> list = new ArrayList<>();
-		list.add(new Teacher(1, "Maria", "12345678987", "987654123", 12000.00));
-		list.add(new Teacher(2, "Marta", "98745612332", "965478123", 11000.00));
-		list.add(new Teacher(3, "Mariana", "36985214723", "965478321", 10000.00));
-		return list;
+		return dao.findAll();
 	}
-
 }
